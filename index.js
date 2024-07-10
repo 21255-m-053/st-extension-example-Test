29d72c2c56bd549e0e09a049c63171e953fd9faa
laxman devi <pfhowcps063@outlook.com>
2024-07-10 15:18:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,57 +7,41 @@ import { extension_settings, getContext, loadExtensionSettings } from "../../../
 //You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
-// Keep track of where your extension is located, name should match repo name
-const extensionName = "st-extension-example";
-const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
+// The main script for the extension focused on enhancing blockquotes
 
+// Import necessary utilities from the extension framework
+import { getContext } from "../../../extensions.js";
 
- 
-// Loads the extension settings if they exist, otherwise initializes them to the defaults.
-async function loadSettings() {
-  //Create the settings if they don't exist
-  extension_settings[extensionName] = extension_settings[extensionName] || {};
-  if (Object.keys(extension_settings[extensionName]).length === 0) {
-    Object.assign(extension_settings[extensionName], defaultSettings);
-  }
-
-  // Updating settings in the UI
-  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
+/**
+ * Adds a copy button to each blockquote element.
+ */
+function addCopyButtonToBlockquotes() {
+  const blockquotes = document.querySelectorAll('blockquote');
+  blockquotes.forEach(blockquote => {
+	const button = document.createElement('button');
+	button.textContent = 'Copy';
+	button.className = 'copy-button';
+	button.onclick = () => copyBlockquoteContent(blockquote);
+	blockquote.insertBefore(button, blockquote.firstChild);
+  });
 }
 
-// This function is called when the extension settings are changed in the UI
-function onExampleInput(event) {
-  const value = Boolean($(event.target).prop("checked"));
-  extension_settings[extensionName].example_setting = value;
-  saveSettingsDebounced();
+/**
+ * Copies the content of the given blockquote to the clipboard.
+ * @param {Element} blockquote The blockquote element.
+ */
+function copyBlockquoteContent(blockquote) {
+  const text = blockquote.innerText;
+  navigator.clipboard.writeText(text).then(() => {
+	console.log('Blockquote content copied to clipboard.');
+  }).catch(err => {
+	console.error('Failed to copy blockquote content: ', err);
+  });
 }
 
-// This function is called when the button is clicked
-function onButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the checked setting
-  toastr.info(
-    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
-    "A popup appeared because you clicked the button!"
-  );
+// Initialize the extension by adding copy buttons to all blockquotes
+function init() {
+  addCopyButtonToBlockquotes();
 }
 
-// This function is called when the extension is loaded
-jQuery(async () => {
-  // This is an example of loading HTML from a file
-  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
-
-  // Append settingsHtml to extensions_settings
-  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
-  // Left should be extensions that deal with system functions and right should be visual/UI related 
-  $("#extensions_settings").append(settingsHtml);
-
-  // These are examples of listening for events
-  $("#my_button").on("click", onButtonClick);
-  $("#example_setting").on("input", onExampleInput);
-
-  // Load settings when starting things up (if you have any)
-  loadSettings();
-});
+init();
